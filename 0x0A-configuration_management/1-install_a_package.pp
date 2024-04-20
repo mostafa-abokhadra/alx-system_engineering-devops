9289fbf4e33692323938ee4e79abe29f0adb52f5
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-20 02:36:41 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Install an especific version of flask (2.1.0)
+package {'Werkzeug':
+  ensure  =>  '2.1.1',
+  provider  =>  'pip3',
+}
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
