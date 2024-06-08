744d6dd855f0e9682fd07df359c42ef1e9b2ec4b
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-09 00:04:21 +0300

    ok

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,28 +2,30 @@
 """requesting and response of api
 """
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-personal_data = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-employee = personal_data.json()
+    user_id = int(sys.argv[1])
+    personal_data = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    employee = personal_data.json()
 
-todo_data = requests.get('https://jsonplaceholder.typicode.com/todos')
-todo_list = todo_data.json()
+    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo_list = todo_data.json()
 
-completed = 0
-total = 0
-completed_list = []
+    completed = 0
+    total = 0
+    completed_list = []
 
-for item in todo_list:
-    if item.get('userId') == int(sys.argv[1]):
-        total += 1
-        if item.get('completed'):
-            completed += 1
-            completed_list.append(item.get('title'))
+    for item in todo_list:
+        if item.get('userId') == int(sys.argv[1]):
+            total += 1
+            if item.get('completed'):
+                completed += 1
+                completed_list.append(item.get('title'))
 
-print("Employee {} is done with tasks({}/{}):".format(
-    employee.get('name'), completed, total))
-for done in completed_list:
-    print("\t " + done)
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get('name'), completed, total))
+    for done in completed_list:
+        print("\t " + done)
