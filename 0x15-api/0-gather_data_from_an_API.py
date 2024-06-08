2c1d9da2b462dc8630924dde4320e4f81c41ba29
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-08 23:53:54 +0300

    ok

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""practicing api"""
+
+import requests
+import sys
+
+personal_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+employee = personal_data.json()
+
+todo_data = requests.get('https://jsonplaceholder.typicode.com/todos')
+todo_list = todo_data.json()
+
+completed = 0
+total = 0
+completed_list = []
+
+for item in todo_list:
+    if item.get('userId') == int(sys.argv[1]):
+        total += 1
+        if item.get('completed'):
+            completed += 1
+            completed_list.append(item.get('title'))
+
+print("Employee {} is done with tasks({}/{}):".format(
+    employee.get('name'), completed, total))
+for done in completed_list:
+    print("\t " + done)
